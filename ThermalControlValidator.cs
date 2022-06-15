346d3bdbb456cca276c0ca926115860138d70ac2
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-06-15 20:31:10 +0530

    Create ThermalControlValidator.cs

---
+++ ThermalControlValidator.cs
@@ -0,0 +1,21 @@
+namespace BatteryManagementSystem
+{
+    using System;
+
+    public class ThermalControlValidator : IThermalControlValidator
+    {
+        public bool CheckIfBatteryTemperatureIsInGivenRange(
+            float minimumTemperature,
+            float maximumTemperature,
+            float currentTemperature)
+        {
+            if (currentTemperature < minimumTemperature || currentTemperature > maximumTemperature)
+            {
+                Console.WriteLine("Temperature is out of range!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
