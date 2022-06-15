f7030bda4837d751eb7594dae542ae3a7f45728b
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-06-15 20:30:50 +0530

    Create IThermalControlValidator.cs

---
+++ IThermalControlValidator.cs
@@ -0,0 +1,10 @@
+namespace BatteryManagementSystem
+{
+    public interface IThermalControlValidator
+    {
+        bool CheckIfBatteryTemperatureIsInGivenRange(
+            float minimumTemperature,
+            float maximumTemperature,
+            float currentTemperature);
+    }
+}
