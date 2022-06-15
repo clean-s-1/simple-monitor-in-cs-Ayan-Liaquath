0c17cfc5e3a8b789505dc79af76f3c1ee186f39f
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-06-15 20:48:29 +0530

    Update StateOfChargeValidator.cs

--- StateOfChargeValidator.cs
+++ StateOfChargeValidator.cs
@@ -1,18 +1,15 @@
-namespace BatteryManagementSystem
+using System;
+
+public class StateOfChargeValidator : IStateOfChargeValidator
 {
-    using System;
-
-    public class StateOfChargeValidator : IStateOfChargeValidator
+    public bool CheckIfStateOfChargeIsInGivenRange(float minimumSoc, float maximumSoc, float currentSoc)
     {
-        public bool CheckIfStateOfChargeIsInGivenRange(float minimumSoc, float maximumSoc, float currentSoc)
+        if (currentSoc < minimumSoc || currentSoc > maximumSoc)
         {
-            if (currentSoc < minimumSoc || currentSoc > maximumSoc)
-            {
-                Console.WriteLine("State of Charge is out of range!");
-                return false;
-            }
-
-            return true;
+            Console.WriteLine("State of Charge is out of range!");
+            return false;
         }
+
+        return true;
     }
 }
