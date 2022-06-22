3d3ab603249dc60c249b87e37d1f9d56cc64113c
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-06-22 22:49:50 +0530

    Update ThermalControlValidator.cs

--- ThermalControlValidator.cs
+++ ThermalControlValidator.cs
@@ -1,21 +1,82 @@
 namespace BatteryManagementSystem
 {
-    using System;
-
-    public class ThermalControlValidator : IThermalControlValidator
+    public class ThermalControlValidator : IBatteryParameterValidator
     {
-        public bool CheckIfBatteryTemperatureIsInGivenRange(
+        private readonly IPrinter _Printer;
+
+        private float _MinimumTemperature;
+
+        private float _MaximumTemperature;
+
+        private readonly bool _IsWarningNeeded;
+
+        private readonly float _WarningPercentage;
+
+        private float _CurrentTemperature;
+
+        public ThermalControlValidator(
             float minimumTemperature,
             float maximumTemperature,
-            float currentTemperature)
+            IPrinter printer,
+            bool isWarningNeeded = true,
+            float warningPercentage = 5)
         {
-            if (currentTemperature < minimumTemperature || currentTemperature > maximumTemperature)
+            _MinimumTemperature = minimumTemperature;
+            _MaximumTemperature = maximumTemperature;
+            _Printer = printer;
+            _IsWarningNeeded = isWarningNeeded;
+            _WarningPercentage = warningPercentage;
+        }
+
+        public void UpdateCurrentParameterValue(float parameterValue)
+        {
+            _CurrentTemperature = parameterValue;
+        }
+
+        public ValidatorResult IsParameterValueValid()
+        {
+            if (_CurrentTemperature < _MinimumTemperature)
             {
-                Console.WriteLine("Temperature is out of range!");
-                return false;
+                _Printer.PrintGivenMessageType(MessageType.MinimumTemperatureLimit);
+                return new ValidatorResult(false, _Printer.FetchMessage(MessageType.MinimumTemperatureLimit));
             }
 
-            return true;
+            if (_CurrentTemperature > _MaximumTemperature)
+            {
+                _Printer.PrintGivenMessageType(MessageType.MaximumTemperatureLimit);
+                return new ValidatorResult(false, _Printer.FetchMessage(MessageType.MaximumTemperatureLimit));
+            }
+
+            CheckForWarnings();
+
+            return new ValidatorResult(true, string.Empty);
+        }
+
+        private void CheckForWarnings()
+        {
+            if (_IsWarningNeeded)
+            {
+                CheckForMaximumLimitWarning();
+                CheckForMinimumLimitWarning();
+            }
+        }
+
+        private void CheckForMaximumLimitWarning()
+        {
+            if (_CurrentTemperature
+                >= WarningValueCalculator.CalculateMaximumWarningLimit(_WarningPercentage, _MaximumTemperature))
+            {
+                _Printer.PrintGivenMessageType(MessageType.MaximumTemperatureWarning);
+            }
+        }
+
+        private void CheckForMinimumLimitWarning()
+        {
+            if (_CurrentTemperature
+                <= WarningValueCalculator.CalculateMinimumWarningLimit(_WarningPercentage, _MinimumTemperature))
+            {
+                _Printer.PrintGivenMessageType(MessageType.MinimumTemperatureWarning);
+            }
         }
     }
 }
