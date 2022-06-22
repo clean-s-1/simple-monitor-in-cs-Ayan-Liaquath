7cb5a6fac26b9f3e06dc1b7bfd0e0c918f9d37b4
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-06-22 22:44:06 +0530

    Update ChargeRateValidator.cs

--- ChargeRateValidator.cs
+++ ChargeRateValidator.cs
@@ -1,18 +1,63 @@
 namespace BatteryManagementSystem
 {
-    using System;
-
-    public class ChargeRateValidator : IChargeRateValidator
+    public class ChargeRateValidator : IBatteryParameterValidator
     {
-        public bool CheckIfChargeRateIsValid(float maximumChargeRate, float currentChargeRate)
+        private readonly IPrinter _Printer;
+
+        private readonly float _MaximumChargeRate;
+
+        private readonly bool _IsWarningNeeded;
+
+        private readonly float _WarningPercentage;
+
+        private float _CurrentChargeRate;
+
+        public ChargeRateValidator(
+            float maximumChargeRate,
+            IPrinter printer,
+            bool isWarningNeeded = true,
+            float warningPercentage = 5)
         {
-            if (currentChargeRate > maximumChargeRate)
+            _MaximumChargeRate = maximumChargeRate;
+            _Printer = printer;
+            _IsWarningNeeded = isWarningNeeded;
+            _WarningPercentage = warningPercentage;
+        }
+
+        public void UpdateCurrentParameterValue(float parameterValue)
+        {
+            _CurrentChargeRate = parameterValue;
+        }
+
+        public ValidatorResult IsParameterValueValid()
+        {
+            if (_CurrentChargeRate > _MaximumChargeRate)
             {
-                Console.WriteLine("Charge Rate is out of range!");
-                return false;
+                _Printer.PrintGivenMessageType(MessageType.MaximumChargeRateLimit);
+
+                return new ValidatorResult(false, _Printer.FetchMessage(MessageType.MaximumChargeRateLimit));
             }
 
-            return true;
+            CheckForWarnings();
+
+            return new ValidatorResult(true, string.Empty);
+        }
+
+        private void CheckForWarnings()
+        {
+            if (_IsWarningNeeded)
+            {
+                CheckForMaximumLimitWarning();
+            }
+        }
+
+        private void CheckForMaximumLimitWarning()
+        {
+            if (_CurrentChargeRate
+                >= WarningValueCalculator.CalculateMaximumWarningLimit(_WarningPercentage, _MaximumChargeRate))
+            {
+                _Printer.PrintGivenMessageType(MessageType.MaximumChargeRateWarning);
+            }
         }
     }
 }
